0c277da8dd87226fcd56197735e3b30fc7d08601
Alejandro Díaz <al.diaz90@gmail.com>
2015-08-19 18:56:04 +0200

    Autenticación y sesión

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -11,6 +12,12 @@ router.get('/', function(req, res) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
+
+// Sesión
+router.get('/login', sessionController.new);
+router.post('/login', sessionController.create);
+router.get('/logout', sessionController.destroy); // debería ser Delete
+
 
 // Definición de rutas de /quizes
 router.get('/quizes',						quizController.index);
